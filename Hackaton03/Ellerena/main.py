07e278e8898ff039e96c23f17d33d5add65aad77
enriquellerena20 <enriquellerenarodriguez@gmail.com>
2023-03-25 13:39:37 -0500

    tarea 03

---
+++ Hackaton03/Ellerena/main.py
@@ -0,0 +1,98 @@
+import solucion
+
+
+
+while True:
+    opcion = int(input("Escoja el ejercicio a revisar: para terminar ponga 999   "))
+    if opcion == 1:
+        solucion.ejercicio01()
+    elif opcion == 2:
+            solucion.ejercicio02()
+    elif opcion == 3:
+        solucion.ejercicio03()
+
+    elif opcion == 4:
+            solucion.ejercicio04()
+
+    elif opcion == 5:
+            solucion.ejercicio05()
+
+    elif opcion == 6:
+            solucion.ejercicio06()
+    elif opcion ==7 :
+            solucion.ejercicio07()
+    elif opcion ==8 :
+            solucion.ejercicio08()
+    elif opcion ==9 :
+            solucion.ejercicio09()       
+    elif opcion ==10 :
+            solucion.ejercicio10()
+    elif opcion ==11 :
+            solucion.ejercicio11()
+    elif opcion ==12 :
+            solucion.ejercicio12()
+    elif opcion ==13 :
+            solucion.ejercicio13()
+    elif opcion ==14 :
+            solucion.ejercicio14()
+    elif opcion ==15 :
+            solucion.ejercicio15()
+    elif opcion ==16 :
+            solucion.ejercicio16()
+    elif opcion ==17 :
+            solucion.ejercicio17()
+    elif opcion == 18:
+            solucion.ejercicio18()
+    elif opcion ==19 :
+            solucion.ejercicio19()
+    elif opcion ==20 :
+            solucion.ejercicio20()
+    elif opcion ==21 :
+            solucion.ejercicio21()       
+    elif opcion ==22 :
+            solucion.ejercicio22()
+    elif opcion == 23:
+            solucion.ejercicio23()
+    elif opcion ==24 :
+            solucion.ejercicio24()
+    elif opcion == 25:
+            solucion.ejercicio25()
+    elif opcion ==26 :
+            solucion.ejercicio26()
+    elif opcion ==27 :
+            solucion.ejercicio27()
+    elif opcion ==28 :
+            solucion.ejercicio28()
+    elif opcion ==29 :
+            solucion.ejercicio29()
+    elif opcion == 30:
+            solucion.ejercicio30()
+    elif opcion ==31 :
+            solucion.ejercicio31()
+    elif opcion ==32 :
+            solucion.ejercicio32()
+    elif opcion ==33 :
+            solucion.ejercicio33()       
+    elif opcion ==34 :
+            solucion.ejercicio34()
+    elif opcion == 35:
+            solucion.ejercicio35()
+    elif opcion ==36 :
+            solucion.ejercicio36()
+    elif opcion ==37 :
+            solucion.ejercicio37()
+    elif opcion ==38 :
+            solucion.ejercicio38()
+    elif opcion ==39 :
+            solucion.ejercicio39()
+    elif opcion == 40:
+            solucion.ejercicio40()
+   
+
+    
+
+    elif opcion == 999:
+        print("gracias por usar el programa  ")
+        break
+    else:
+        print("No implementado")
