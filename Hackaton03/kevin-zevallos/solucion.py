345d6dea21adac8905423f8d16ea5cfe60da5141
kevin1892 <kevin.zevallos@utec.edu.pe>
2023-03-25 09:53:49 -0500

    5 primeros ejercicios completados

--- Hackaton03/kevin-zevallos/solucion.py
+++ Hackaton03/kevin-zevallos/solucion.py
@@ -1,13 +1,44 @@
 def ejercicio01():
-    pass
+    numero = int(input('ingrese un numero para determinar la cantidad de dígitos: '))
+    cant_digitos = len(str(numero))
+    print(f'el número {numero} tiene {cant_digitos} dígitos')
 def ejercicio02():
-    pass
+    numero = int(input('ingrese un numero para determinar si es negativo: '))
+    if numero > 0:
+        print(f'el número {numero} es positivo')
+    else:
+        print(f'el número {numero} es negativo')
 def ejercicio03():
-    pass
+    numero = int(input("Ingrese un número: "))
+    if numero % 10 == 4:
+        print("El número termina en 4.")
+    else:
+        print("El número no termina en 4.")
+
 def ejercicio04():
-    pass
+    print('ingrese 3 numeros: ')
+    n1 = int(input('número 1: '))
+    n2 = int(input('número 2: '))
+    n3 = int(input('número 3: '))
+    numeros = [n1,n2,n3]
+    numeros.sort()
+    print('los números ordenados de menor a mayor son :')
+    for num in numeros:
+        print(num)
 def ejercicio05():
-    pass
+    zapatos = int(input("Ingrese el número de zapatos a comprar: "))
+    precio_sin_descuento = zapatos * 80
+    if zapatos > 10 & zapatos <=20:
+        descuento = 0.1
+    elif zapatos > 20 & zapatos<30:
+        descuento = 0.2
+    if zapatos > 30:
+        descuento = 0.4
+    else:
+        descuento = 0
+    precio_con_descuento = precio_sin_descuento * (1 - descuento)
+    print(f"El precio total de la compra es: {precio_con_descuento} con descuento de {descuento}")
+
 def ejercicio06():
     pass
 def ejercicio07():
