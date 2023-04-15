ed33a12bb068b2fd2a554e8ad3b7e0a871b9f6f5
Maritza Rodriguez <maritzarg2020@gmail.com>
2023-04-14 23:33:26 -0500

    fix

--- Semana06Sesion02/mrodriguez/conexion.py
+++ Semana06Sesion02/mrodriguez/conexion.py
@@ -3,16 +3,47 @@ from psycopg2 import Error
 
 
 class conexion:
-    def __init__(self):
+    def conexion(self):
         try:
             conn = psycopg2.connect(
                 user="postgres",
                 password="pSecret6#",
                 host="127.0.0.1",
-                port=5432,
+                port="5432",
                 database="hackathon05",
             )
             return conn
         except Error as error:
-            print(f"Ha ocurrido un error, {error}")
+            print(f"Ha ocurrido en error: {str(error)}")
             return False
+
+    def consultarBDD(self, query):
+        try:
+            conexion = self.conexion()
+            cur = conexion.cursor()
+            cur.execute(query)
+            records = cur.fetchall()
+            return records
+        except Error as error:
+            print(f"Ha ocurrido un error: {str(error)}")
+            return False
+        finally:
+            if conexion:
+                cur.close()
+                conexion.close()
+
+    def ejecutarBDD(self, query):
+        try:
+            conexion = self.conexion()
+            cur = conexion.cursor()
+            cur.execute(query)
+            conexion.commit()
+            exito = True
+            return exito
+        except Error as error:
+            print(f"Ha ocurrido un error: {str(error)}")
+            return False
+        finally:
+            if conexion:
+                cur.close()
+                conexion.close()
