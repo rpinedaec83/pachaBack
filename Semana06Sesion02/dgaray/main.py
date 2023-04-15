142be5cd9b48b1318bf83da3d40819540e1215a7
Dieguidev <diegogaraycullas@gmail.com>
2023-04-14 20:26:50 -0500

    conexion a base de datos con python

---
+++ Semana06Sesion02/dgaray/main.py
@@ -0,0 +1,21 @@
+from psycopg2 import connect, Error
+
+try:
+    conn = connect(
+        user = 'postgres', 
+        password = '021087Mikeyla', 
+        host='127.0.0.1', 
+        port = '5433', 
+        database = 'postgres')
+    cur = conn.cursor()
+    query = 'select version();'
+    cur.execute(query)
+    res = cur.fetchone()
+    print(res)
+
+except(Exception, Error) as error:
+    print(f"Hubo un error en la conexion {str(error)}")
+finally:
+    cur.close()
+    conn.close()
+    print(f'Se ha cerrado la conexion')
