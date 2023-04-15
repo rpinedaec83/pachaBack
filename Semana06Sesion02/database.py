07409dba1878e68153ece4448fb5948d68965f64
Herless Acuña <herlessacuna@gmail.com>
2023-04-14 22:40:40 -0500

    ClaseRecup

---
+++ Semana06Sesion02/database.py
@@ -0,0 +1,12 @@
+from psycopg2 import connect
+
+host = "0.0.0.0"
+port = 5432
+dbname = "prueba"
+user = "postgres"
+password = "zagher99"
+
+def cadena_conexion():
+    conexion = connect(host = host, port = port, dbname = dbname, user = user, password = password )
+
+    return conexion
