d8048402a1b9ab96d1993f9f88ec60c2c4c901e1
Dieguidev <diegogaraycullas@gmail.com>
2023-04-15 15:06:12 -0500

    CRUDs cursos y salones

--- Hackaton06/dgaray/main.salon.py
+++ Hackaton06/dgaray/main.salon.py
@@ -16,43 +16,49 @@ try:
             break
         if(ansMenuPrincipal == "1"):
             header = ['ID','anoEscolar','seccion', 'idAlumno', 'idProfesor']
-            query = "Select * from profesores;"
+            query = "Select * from salon;"
             res = conexion.consultarBDD(query=query)
             print(tabulate(res, headers=header, tablefmt='fancy_grid'))
             input("Desea contunuar??")
         if(ansMenuPrincipal == "2"):
-            idProfesor = int(input("Que profesores deseas editar: "))
-            query = f"Select * from profesores where id = {idProfesor};"
+            idSalon = int(input("Que salon deseas editar: "))
+            query = f"Select * from salon where id = {idSalon};"
             res = conexion.consultarBDD(query=query)
             header = ['ID','anoEscolar','seccion', 'idAlumno', 'idProfesor']
             print(tabulate(res, headers=header, tablefmt='fancy_grid'))
             input("Desea contunuar??")
-            nombreProfesor = input("Dime el nombre del profesor: ")
-            email = input("Dime tu email: ")
-            query = f'''Update profesores 
-                set nombre = '{nombreProfesor}',
-                email = '{email}'
-                where id = {idProfesor};'''
+            anoEscolar = int(input("Dime el año escolar: "))
+            seccion = input("Dime la seccion: ")
+            idAlumno = int(input("Dime el id del alumno: "))
+            idProfesor = int(input("Dime el id del profesor: "))
+            query = f'''Update salon 
+                set ano_escolar = {anoEscolar},
+                seccion = '{seccion}',
+                id_alumno = {idAlumno},
+                id_profesor = {idProfesor}
+                where id = {idSalon};'''
             if(conexion.ejecutarBDD(query)):
                 print("Datos actualizados correctamente")
                 input("Desea continuar???")
         if(ansMenuPrincipal=="3"):
-            nombreProfesor = input("Dime el nombre del profesor: ")
-            email = input("Dime tu email: ")
-            query= f'''insert into profesores(nombre,email)
-                     values('{nombreProfesor}','{email}');'''
+            anoEscolar = int(input("Dime el año escolar: "))
+            seccion = input("Dime la seccion: ")
+            idAlumno = int(input("Dime el id del alumno: "))
+            idProfesor = int(input("Dime el id del profesor: "))
+            query= f'''insert into salon(ano_escolar,seccion,id_alumno,id_profesor)
+                     values({anoEscolar},'{seccion}', {idAlumno}, {idProfesor});'''
             if(conexion.ejecutarBDD(query)):
                 print("Datos creados correctamente")
                 input("Desea continuar???")
         if(ansMenuPrincipal == "4"):
-            idProfesor = int(input("Que profesor deseas borrar: "))
-            query = f"Select * from profesores where id = {idProfesor};"
+            idSalon = int(input("Que salon deseas borrar: "))
+            query = f"Select * from salon where id = {idSalon};"
             res = conexion.consultarBDD(query=query)
             header = ['ID','anoEscolar','seccion', 'idAlumno', 'idProfesor']
             print(tabulate(res, headers=header, tablefmt='fancy_grid'))
             input("Desea contunuar??")
-            query = f''' delete from profesores
-                where id = {idProfesor};'''
+            query = f''' delete from salon
+                where id = {idSalon};'''
             if(conexion.ejecutarBDD(query)):
                 print("Datos borrados correctamente")
                 input("Desea continuar???")
