77c7632ec772e76e6e2b0f857da04dd0b4d0965c
Alonso Ulloa <grisol225@gmail.com>
2023-04-15 19:04:26 -0500

    hackaton6

---
+++ Hackaton06/AULLOA/alumnos.py
@@ -0,0 +1,6 @@
+class Alumnos:
+    def __init__(self, IdAlumno, nombreAlumno, emailAlumno, edadAlumno):
+        self.IdAlumno = IdAlumno
+        self.nombreAlumno = nombreAlumno
+        self.emailAlumno = emailAlumno
+        self.edadAlumno = edadAlumno
