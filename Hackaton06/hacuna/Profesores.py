764236e8ec255b559b8e55d4ffbfde48d86c230a
Herless Acuña <herlessacuna@gmail.com>
2023-04-18 11:37:56 -0500

    TareasHackaton6

---
+++ Hackaton06/hacuna/Profesores.py
@@ -0,0 +1,9 @@
+class Profesores():
+    
+    def __init__(self, id, nombre, identificador, edad, correo):
+        self.idAlumno = id
+        self.nombre = nombre
+        self.identificador = identificador
+        self. edad = edad
+        self.correo = correo
+        
