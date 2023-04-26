2d52d1d737dae3aa7907cb82615df8d99c39b0c1
Roberto Pineda <rpineda@x-codec.net>
2023-04-25 21:33:57 -0500

    Clase del 25/04/2023 MVC

---
+++ Semana08Sesion01/rpineda/model.py
@@ -0,0 +1,26 @@
+from db import db
+class Libros:
+    def __init__(self, titulo, fecPublicacion, isbn, autor):
+        self.titulo = titulo
+        self.fecPublicacion = fecPublicacion
+        self.isbn = isbn
+        self.autor = autor
+
+    def crearLibro(self):
+        conn = db()
+        query = f'''INSERT INTO public."Libros"(
+	                 titulo, "fecPublicacion", isbn, autor)
+                    values('{self.titulo}', '{self.fecPublicacion}', '{self.isbn}', {self.autor})'''
+        return conn.ejecutarBDD(query)
+    
+    def traerLibros(self):
+        conn = db()
+        query = f"select * from Libros"
+        return conn.consultarBDD(query)
+    
+    def traerLibro(self):
+        conn = db()
+        query = f"Select * from Libros where nombre = '{self.nombre}'"
+        return conn.consultarBDD(query)
+    
+
