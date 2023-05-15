19703ec81859351fd5c9d05fd151300c0a3f6e6c
kevin1892 <kevin.zevallos@utec.edu.pe>
2023-05-14 22:48:46 -0500

    migraciones creadas

---
+++ Hackaton10/kevin_zevallos/migrations/2023_05_15_022648_create_usuarios_table.py
@@ -0,0 +1,20 @@
+from orator.migrations import Migration
+
+
+class CreateUsuariosTable(Migration):
+
+    def up(self):
+        """
+        Run the migrations.
+        """
+        with self.schema.table('usuarios') as table:
+            table.increments('id')
+            table.string('nombre')
+            table.string('rol')
+            table.timestamps()
+
+    def down(self):
+        """
+        Revert the migrations.
+        """
+        self.schema.drop('usuarios')
