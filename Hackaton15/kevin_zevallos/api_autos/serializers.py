9987a440f48327c53e66210efb36c03eaf8cf96e
mclovin <kevin.zevallos@utec.edu.pe>
2023-06-18 23:24:51 -0500

    agregando rutas de crud de autos

--- Hackaton15/kevin_zevallos/api_autos/serializers.py
+++ Hackaton15/kevin_zevallos/api_autos/serializers.py
@@ -2,15 +2,15 @@ from rest_framework import serializers
 from .models import Tipo,Marca,Auto
 class TipoSerializer(serializers.ModelSerializer):
     class Meta:
-        model='Tipo'
+        model=Tipo
         fields = '__all__'
 
 class MarcaSerializer(serializers.ModelSerializer):
     class Meta:
-        model='Marca'
+        model=Marca
         fields = '__all__'
 
 class AutoSerializer(serializers.ModelSerializer):
     class Meta:
-        model='Auto'
+        model=Auto
         fields = '__all__'
