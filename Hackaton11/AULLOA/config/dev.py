6f5a9f55f457847cb811d4f30f4c4acecf06c98b
Alonso Ulloa <grisol225@gmail.com>
2023-07-08 12:54:13 -0500

    hackaton11

---
+++ Hackaton11/AULLOA/config/dev.py
@@ -0,0 +1,5 @@
+from .default import *
+
+TESTING = False
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123@localhost:5433/minimarket'
---
+++ Hackaton11/AULLOA/config/prod.py
@@ -0,0 +1,3 @@
+from .default import *
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://gixpzqli:K62fuSl68EXKFM2znhexV9WXTrReBMvS@drona.db.elephantsql.com/gixpzqli'
